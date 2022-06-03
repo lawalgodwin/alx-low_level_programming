610fbb194aee2e1db89ccdce205afae6622afcfc
lawalgodwin <godwinl200@gmail.com>
2022-06-02 19:13:38 -0700

    Print string to the terminal using puts in C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+  * main - entry point
+  *
+  * Description - Printing string to the terminal using puts function
+  *
+  * Return: Always return 0 (Success)
+  *
+  */
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle");
+return (0);
+
+}
