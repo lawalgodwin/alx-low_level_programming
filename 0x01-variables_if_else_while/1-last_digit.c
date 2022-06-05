9d1b4e23fedc552ca2e097a7df306b0ac4cc180d
lawalgodwin <godwinl200@gmail.com>
2022-06-05 02:28:41 -0700

    Get the last digit of a number

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,13 +18,18 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	last_digit = n % 10;
-	if (last_digit > 5)
-	   printf("Last digit of %i is %i and is greater than 5\n",\
-			   n, last_digit);
 	if (last_digit == 0)
-	   printf("Last digit of %i is %i and is 0\n", n, last_digit);
+		printf("Last digi of %i is %i and is 0\n", n, last_digit);
 	if (last_digit < 6 && last_digit != 0)
-	   printf("Last digit of %i is %i and is less than 6 and not 0\n",\
-			   n, last_digit);
+	{
+		printf("Last digit of %i is %i and is less than 6 and not 0", n, last_digit);
+		printf("\n");
+	}
+	if (last_digit > 5)
+	{
+		printf("Last digit of %i is %i and is greater than 5", n, last_digit);
+		printf("\n");
+	}
+
 	return (0);
 }
