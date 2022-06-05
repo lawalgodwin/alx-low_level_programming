4c41758cbf25aefaa8f2057ddd411e0321f2546e
lawalgodwin <godwinl200@gmail.com>
2022-06-05 04:18:06 -0700

    Print 0-9 using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Return: Always returns 0 (Success)
+  *
+  */
+int main(void)
+{
+	unsigned char i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i + '0');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
