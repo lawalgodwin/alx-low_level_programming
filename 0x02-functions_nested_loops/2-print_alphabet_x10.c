34d60ec6547dcedbe0b5d3290f8b36bb5c7a0a21
lawalgodwin <godwinl200@gmail.com>
2022-06-07 23:10:34 +0000

    Print a-z x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - printa a-z x10 one per line
+ *
+ * Return: Always nothing
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 1; i <= 10; i++)
+	{
+		print_alphabet();
+	}
+
+}
