0f77c860e5769661a1ca72ddff17e8f7a559d574
Lawal Godwin <godwinl200@gmail.com>
2022-09-15 09:53:53 -0700

    Check if letter is uppercase

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN
+#define MAIN
+
+int _putchar(char c);
+
+int _islower(int c);
+
+int print_sign(int n);
+
+int _abs(int);
+
+int _isalpha(int c);
+
+void print_alphabet(void);
+
+int _isupper(int c);
+
+void print_alphabet_x10(void);
+
+#endif /* MAIN */
