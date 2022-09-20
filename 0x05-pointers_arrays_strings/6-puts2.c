273d35017e4124c059e17cf2063fdbf7d5e0f6db
Lawal Godwin <godwinl200@gmail.com>
2022-09-20 04:06:45 -0700

    Filter out the even numbers

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * puts2 - print only char divisible by 2
+  *
+  * @str: The string to be filtered
+  *
+  * Return: returns nothing
+  *
+  */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; *(str + i) != '\0'; i++)
+		if ((*(str + i) % 2 == 0))
+			_putchar(*(str + i));
+	_putchar('\n');
+
+}
