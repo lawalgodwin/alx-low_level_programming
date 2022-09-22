812583fdce33e3d43907ea637eaa7287650e906c
Lawal Godwin <godwinl200@gmail.com>
2022-09-22 10:50:29 -0700

    Copy string based on number of bytes

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+/**
+  * _strncpy - Copy strings based on number of bytes
+  * @n: The number of bytes
+  * @dest: The destination
+  * @src: The source
+  *
+  * Return: returns the pointer to the copy
+  *
+  */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while ((*(dest++) = *(src++)) && (i < n))
+		;
+
+	return (dest);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,6 +3,8 @@
 
 int _putchar(int c);
 
+char *_strncpy(char *dest, char *src, int n);
+
 char *_strcat(char *dest, char *src);
 
 char *_strncat(char *dest, char *src, int n);
