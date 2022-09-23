44b86a7ae85a20557ce93a314f24bc12add2c51b
Lawal Godwin <godwinl200@gmail.com>
2022-09-22 17:02:39 -0700

    Compare two strings and return their diffence

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -5,6 +5,8 @@ int _putchar(int c);
 
 char *_strncpy(char *dest, char *src, int n);
 
+int _strcmp(char *s1, char *s2);
+
 char *_strcat(char *dest, char *src);
 
 char *_strncat(char *dest, char *src, int n);
