488805278b5ac4099eb1055657da42e60248ba3d
Lawal Godwin <godwinl200@gmail.com>
2022-09-27 04:26:44 -0700

    Locate a character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+/**
+  * _strchr - Locate a character in a string
+  *
+  * @s: The string to search
+  *
+  * @c: The character to find
+  *
+  * Return: Returns the memory location of the first occurence of `c`
+  */
+
+char *_strchr(char *s, char c)
+{
+	int position = 0;
+
+	for (; *(s + position); position++)
+	{
+		if (*(s + position) == c)
+
+			return (s + position);
+	}
+
+	return (NULL);
+}
