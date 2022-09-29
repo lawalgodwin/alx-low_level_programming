572f79905b0a15a7088128f4321f8d9d93e3ac3f
Lawal Godwin <godwinl200@gmail.com>
2022-09-28 21:44:43 -0700

    Print strings using recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,23 @@
+/**
+  * _puts_recursion - echo strings using recursion
+  *
+  * @s: The string to print
+  *
+  * Return: returns nothing
+  */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		putchar('\n');
+
+		return;
+	}
+
+	putchar(*s);
+
+	s++;
+
+	_puts_recursion(s);
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,3 @@
+# Recursion
+
+This directory contains solution to task using Recursion instead of Iteration
