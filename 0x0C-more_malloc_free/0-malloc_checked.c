f296456f8333ad2e4f2cb86967bf11871af7b80e
Lawal Godwin <godwinl200@gmail.com>
2022-10-07 02:56:26 -0700

    Check if memory was successfully allocated

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+
+#include "main.h"
+
+/**
+  * malloc_checked - Check if memory was sucessfully allocated by the OS
+  *
+  * @b: The size of memory in bytes to be allocated
+  *
+  * Return: returns the pointer to the reserved memory
+  */
+
+void *malloc_checked(unsigned int b)
+
+{
+	void *memory;
+
+	if ((memory = malloc(b)) == NULL)
+
+		exit(98);
+
+	return (memory);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,4 @@
+# More on dynamic memory allocation
+
+This directory contains the solutions the alx task on advance memory usage
+
