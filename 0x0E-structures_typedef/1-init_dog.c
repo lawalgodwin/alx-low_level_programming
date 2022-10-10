2ab4375468c4d2223422ea734983acb655534fcd
Lawal Godwin <godwinl200@gmail.com>
2022-10-10 11:17:33 -0700

    Initilize the members of a struct

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,7 +2,6 @@
 
 #include "dog.h"
 
-
 /**
   * init_dog - The fuction initialises object of type dog
   *
@@ -29,4 +28,5 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 	d->age = age;
 
 	d->owner = owner;
+
 }
