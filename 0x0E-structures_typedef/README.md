ccb2f306f9f8f8c9d5741d17d58492f6468d14a5
Lawal Godwin <godwinl200@gmail.com>
2022-10-10 09:55:25 -0700

    Describe an object of type dog

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,4 @@
+# Introduction to Structures
+
+The directory contains introductory concepts to C Structures
+
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,24 @@
+#ifndef DOG_H
+
+#define DOG_H
+
+/**
+  * struct dog - A description of an object of type dog
+  *
+  * @name: the name of the dog
+  * @age: The age of the dog
+  * @owner: The owner of the dog
+  *
+  */
+
+struct dog
+
+{
+	char *name;
+
+	int age;
+
+	char *owner;
+}
+
+#endif /*DOG_H*/
