947e729a1adf990a6872511aa3d8b2fb3ddc750b
Lawal Godwin <godwinl200@gmail.com>
2022-10-14 11:07:17 -0700

    Iterate an array via a function pointer

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,24 @@
+/**
+  * array_iterator - Iterates throuh an array using a funtion Ptr
+  *
+  * @array: The array to iterate
+  * @size: The size of the array
+  * @action: the function pointer
+  *
+  */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i = 0;
+
+	if (!array || !action)
+
+		return;
+
+	while (i < size)
+	{
+		action(*(array + i));
+
+		i++;
+	}
+}
