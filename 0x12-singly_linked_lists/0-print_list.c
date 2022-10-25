e8c4f37cb526a711f2d8dc682715d865294cf3b0
Lawal Godwin <godwinl200@gmail.com>
2022-10-25 01:53:27 -0700

    Print the elements of a singly linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,34 @@
+#include "list.h"
+
+/**
+  * print_list - Prints all the elements of a list_t list.
+  *
+  * @h: the list to print its elements
+  *
+  * Return: returns the number of elements in the list
+  */
+
+size_t print_list(const list_t *h)
+{
+	list_t *currentnode = h;
+
+	size_t nodecount = 0;
+
+	while (currentnode != NULL)
+	{
+		nodecount++;
+
+		if (currentnode->str == NULL)
+
+			printf("[0] (nil)\n");
+
+		else
+		{
+			printf("[%d] %s\n", currentnode->len, currentnode->str);
+		}
+
+		currentnode = currentnode->next;
+	}
+
+	return (nodecount);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,5 @@
+# DATA STRUCTURES
+
+Dealing with singly linked list
+
+This repo contains the ALX SE mandatory tasks on Singly Linked list
