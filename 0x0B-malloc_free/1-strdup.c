068e52f62bebfe1896f7beb00bf8cf43d107a27f
Lawal Godwin <godwinl200@gmail.com>
2022-10-30 01:00:46 -0700

    Duplicate a string and store in a dynamically allocated space

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+  * _strdup - Duplicate a string and store it in a dynamic memory
+  *
+  * @str: The string to be duplicated
+  *
+  * Return: Returns the address of the first byte of the string
+  *
+  */
+
+char *_strdup(char *str)
+{
+	char *ptr;
+
+	int i, j = 0;
+
+	if (str == NULL)
+
+		return (NULL);
+
+	for (i = 0; *(str + i); i++)
+		;
+
+	ptr = malloc((i + 1) * sizeof(char));
+
+	if (ptr == NULL)
+
+		return (NULL);
+
+	while (j < i)
+	{
+		*(ptr + j) = *(str + j);
+
+		j++;
+	}
+
+	return (ptr);
+}
