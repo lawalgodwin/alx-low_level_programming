e9760b5d2c18f456736db4ef978fe03bb0624222
Lawal Godwin <godwinl200@gmail.com>
2022-11-19 07:24:52 -0800

    Convert binary to integer

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+  * binary_to_uint - Converts binary strings to an unsigned integer
+  *
+  * @b: The binary string to convert
+  *
+  * Return: returns the convertion result
+  */
+
+unsigned int binary_to_uint(const char *b)
+{
+	const char *p = b;
+
+	unsigned int sum = 0, power = 0;
+
+	while (*(b++))
+		b++;
+
+	b--;
+
+	while (b >= p)
+	{
+		if (*b != '0' && *b != '1')
+
+			return (0);
+
+		sum += (*b - '0') * (1 << power);
+
+		power++;
+
+		b--;
+	}
+
+	return (sum);
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,3 @@
+# Bit Manipulation
+
+The project conttains bit manipulation using the C language and some usecases
