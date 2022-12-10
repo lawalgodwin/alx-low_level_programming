1207184955da42510bc767ff694e543d0a2efc94
lawalgodwin <godwinl200@gmail.com>
2022-12-10 11:13:38 +0000

    Get the node in a list at a given index

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+
+#include "lists.h"
+
+/**
+  * get_dnodeint_at_index - Get the node at a specified index in a list
+  *
+  * @index: the index and it starts from 0
+  *
+  * @head: The head of the list
+  *
+  * Return: The node at the given index
+  */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *target_node;
+
+	unsigned int count = 0;
+
+	if (head == NULL)
+
+		return (NULL);
+
+	while (head != NULL)
+	{
+		if (count == index)
+		{
+			target_node = head;
+
+			break;
+		}
+
+		count = count + 1;
+
+		head = head->next;
+	}
+
+	return (target_node);
+}
