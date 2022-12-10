0d24b3ede3a516442859bb295ef8e0a1e160f83d
lawalgodwin <godwinl200@gmail.com>
2022-12-10 11:21:44 +0000

    Get the node in a list at a given index

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -21,6 +21,12 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	if (head == NULL)
 
 		return (NULL);
+	
+	if (head != NULL)
+
+		while (head->prev != NULL)
+
+			head = head->prev;
 
 	while (head != NULL)
 	{
