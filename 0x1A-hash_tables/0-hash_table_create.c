c63f92653d6b789202524850dd15a34089bcc8c9
lawalgodwin <godwinl200@gmail.com>
2023-01-30 11:18:09 +0100

    Create a hash table of a size

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,7 +8,7 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *ht = NULL;
+	hash_table_t *ht;
 
 	unsigned long int i = 0;
 
@@ -20,7 +20,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	ht->size = size;
 
-	ht->array = malloc(sizeof(hash_node_t) * size);
+	ht->array = malloc(sizeof(hash_node_t *) * size);
 
 	if (ht->array == NULL)
 
