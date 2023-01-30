0806458e5f1c89c246807fa56ff70044b00d8b48
lawalgodwin <godwinl200@gmail.com>
2023-01-30 11:08:50 +0100

    Create a hash table of a size

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -23,11 +23,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 	ht->array = malloc(sizeof(hash_node_t) * size);
 
 	if (ht->array == NULL)
-	{
-		free(ht);
 
 		return (NULL);
-	}
 
 	for (; i < size; i++)
 	{
