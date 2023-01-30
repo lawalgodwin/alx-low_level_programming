84fa9d7637cd5945d18651ef92aa95be34a3468d
lawalgodwin <godwinl200@gmail.com>
2023-01-30 17:12:20 +0100

    Insert item into a hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -58,7 +58,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 /**
   * update_htable - Updates the hash table with new value if key already exist
-  * @ht: The hash table pointer
+  * @item: The hash table pointer
   * @key: the key pointer
   * @value: the new value pointer
   * Return: 1 if updated 0 otherwise
