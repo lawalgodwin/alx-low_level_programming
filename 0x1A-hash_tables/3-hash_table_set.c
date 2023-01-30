1c67a2439f47313aeb0adc2735bfc5c2a464e304
lawalgodwin <godwinl200@gmail.com>
2023-01-30 14:50:00 +0100

    Insert item into a hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -28,9 +28,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 		return (0);
 
-	item->key = (char *)key;
+	item->key = strdup(key);
 
-	item->value = (char *)value;
+	item->value = strdup(value);
 
 	index = key_index((unsigned char *)key, ht->size);
 
